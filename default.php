44dc5a0084ff00f2c5cc11a57481d9cc228fe237
wahldev.com <wahldev.com@n21.c06.mtsvc.net>
2015-07-07 07:17:05 -0700

    Status quo site

---
+++ default.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title>Hito Bodyworks</title>
+<meta name="description" content="Hito Bodyworks has existed since 1997 with the intention of promoting health and well-being in a way that supports the inner truth of each individual.">
+<meta name="keywords" content="Hito Bodyworks, bodywork, Minneapolis, Minnesota, massage, trager psychophysical integration, trager, CranioSacral Therapy, MC Kunz">
+</head>
+<body style="background-color: #b5a5cf;">
+
+<center>
+<div style="font-family: verdana; font-size: 20px; padding-top: 200px;">
+<img src="Hito-logo.png" alt="Hito Bodyworks"><br><br>
+Coming soon.
+
+</div>
+</center>
+
+
+
+</body>
+</html>
---
+++ wp-content/themes/hito/template-homepage.php
@@ -0,0 +1,81 @@
+<?php
+/*
+Template Name: Homepage
+*/
+?>
+
+<?php
+
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+
+/**
+ * Pages Template
+ *
+ *
+ * @file           page.php
+ * @package        Responsive 
+ * @author         Emil Uzelac 
+ * @copyright      2003 - 2013 ThemeID
+ * @license        license.txt
+ * @version        Release: 1.0
+ * @filesource     wp-content/themes/responsive/page.php
+ * @link           http://codex.wordpress.org/Theme_Development#Pages_.28page.php.29
+ * @since          available since Release 1.0
+ */
+
+get_header(); ?>
+
+<div id="content" class="<?php echo implode( ' ', responsive_get_content_classes() ); ?>">
+
+	<?php if (have_posts()) : ?>
+
+		<?php while (have_posts()) : the_post(); ?>
+        
+        <?php get_template_part( 'loop-header' ); ?>
+        
+			<?php responsive_entry_before(); ?>
+			<div id="post-<?php the_ID(); ?>" class="home_container">       
+				<?php responsive_entry_top(); ?>
+
+                
+				<!-- #featured-image -->
+				<div id="featured-image" class="grid col-380 fit">
+					<?php 
+					if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+					  the_post_thumbnail('medium');
+					} 
+					?>											
+				</div><!-- end of #featured-image --> 
+
+                <div id="grid col-540" class="homepage">
+                    <?php the_content(__('Read more &#8250;', 'responsive')); ?>
+                    <?php wp_link_pages(array('before' => '<div class="pagination">' . __('Pages:', 'responsive'), 'after' => '</div>')); ?>
+                </div><!-- end of .post-entry -->
+            
+				<?php get_template_part( 'post-data' ); ?>
+				               
+				<?php responsive_entry_bottom(); ?>      
+			</div><!-- end of #post-<?php the_ID(); ?> -->       
+			<?php responsive_entry_after(); ?>
+            
+			<?php responsive_comments_before(); ?>
+			<?php comments_template( '', true ); ?>
+			<?php responsive_comments_after(); ?>
+            
+        <?php 
+		endwhile; 
+
+		get_template_part( 'loop-nav' ); 
+
+	else : 
+
+		get_template_part( 'loop-no-posts' ); 
+
+	endif; 
+	?>  
+      
+</div><!-- end of #content -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
